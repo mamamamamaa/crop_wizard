290a52189a7b368633f4b334586c375935584f1c
mamamama <maks.denishchuk@gmail.com>
2023-04-14 13:14:19 +0300

    add

--- components/Crop.tsx
+++ components/Crop.tsx
@@ -1,14 +1,21 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 import Cropper from "cropperjs";
 import Image from "next/image";
 import Monkey from "../public/monkey.jpg";
 
 export const Crop: FC = () => {
+  const imageRef = useRef(null);
   const cropper = new Cropper();
+
+  useEffect(() => {
+    if (imageRef.current) {
+      imageRef.current.focus();
+    }
+  }, []);
 
   return (
     <>
-      <Image src={Monkey} alt="Monkey image" />
+      <Image src={Monkey} alt="Monkey image" ref={imageRef} />
     </>
   );
 };
