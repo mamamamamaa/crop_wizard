e76db21363845ae8d164a185013fb8afc506e2b8
mamamama <maks.denishchuk@gmail.com>
2023-04-15 23:55:10 +0300

    fix up app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,10 @@ import { StoreProvider } from "@/lib/StoreProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <StoreProvider {...pageProps.initialZustandState}>
-      <Component {...pageProps} />
-    </StoreProvider>
+    <>
+      <StoreProvider {...pageProps.initialZustandState}>
+        <Component {...pageProps} />
+      </StoreProvider>
+    </>
   );
 }
