3562a27e16fd09ed618d858c12e7d2012254c3df
mamamama <maks.denishchuk@gmail.com>
2023-04-20 16:05:26 +0300

    fix up

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,17 @@ import { ReactElement } from "react";
 import { Layout } from "@/components/Layout/Layout";
 
 const Home: NextPageWithLayout = () => {
-  const { isLoading, email } = useStore((state) => state.auth.data);
+  // const { isLoading, email } = useStore((state) => state.auth.data);
+
+  const googleAuthLink = `${process.env.SERVER}/api/auth/google/callback`;
 
   return (
     <div>
       <Head>
         <title>CropWizard</title>
       </Head>
+
+      <a href={googleAuthLink}>Google auth</a>
     </div>
   );
 };
